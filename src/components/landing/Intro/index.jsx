f5ebcf0e94fe051d85b75dd8ae2771443c0ee89e
varun <varunvaru27101999@gmail.com>
2021-07-07 01:05:33 +0530

    added new svg for intro section

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -1,28 +1,31 @@
-import React, { useContext } from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { ThemeContext } from 'providers/ThemeProvider';
-import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/dev.svg';
-import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles';
+import React, { useContext } from "react"
+import { ThemeContext } from "providers/ThemeProvider"
+import { Container } from "components/common"
+import dev from "assets/illustrations/dev.svg"
+import { Wrapper, IntroWrapper, Details, Thumbnail } from "./styles"
 
 export const Intro = () => {
-  const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext)
 
   return (
     <Wrapper>
-      
       <IntroWrapper as={Container}>
         <Details theme={theme}>
-          <h1>Hi There!</h1>
-          <h4>I’m Varun Srivathsa and I’m a Student and Freelancer pursuing a career in Data Science!</h4>
-          <Button as={AnchorLink} href="#contact">
-            Contact Me
-          </Button>
+          <h1>
+            Hi There!{" "}
+            <span role="img" aria-labelledby="greeting">
+              👋
+            </span>
+          </h1>
+          <h4>
+            I’m Varun Srivathsa, A Student and Freelancer pursuing a career in
+            Data Science.
+          </h4>
         </Details>
         <Thumbnail>
           <img src={dev} alt="I’m John and I’m a JAMStack engineer!" />
         </Thumbnail>
       </IntroWrapper>
     </Wrapper>
-  );
-};
+  )
+}
