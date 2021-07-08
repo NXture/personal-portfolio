d533870756f67168c2a6a9c6d0c6a5c0d40ca2b5
varun <varunvaru27101999@gmail.com>
2021-07-08 13:18:52 +0530

    added syntax highlight feature for codeblocks in mdx

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,27 @@
 
 import React from "react"
 import ThemeProvider from "providers/ThemeProvider"
+import { MDXProvider } from "@mdx-js/react"
+import { Code } from "./src/CodeBlock"
+import { preToCodeBlock } from "mdx-utils"
+
+export const onServiceWorkerUpdateReady = () => window.location.reload(true)
+
+const components = {
+  pre: preProps => {
+    const props = preToCodeBlock(preProps)
+    // if there's a codeString and some props, we passed the test
+    if (props) {
+      return <Code {...props} />
+    } else {
+      // it's possible to have a pre without a code in it
+      return <pre {...preProps} />
+    }
+  },
+}
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
+  <ThemeProvider>
+    <MDXProvider components={components}>{element}</MDXProvider>
+  </ThemeProvider>
 )
