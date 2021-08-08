fb10a0540c1e29dbabe559000a77a8f17fb21462
NXture <varunvaru27101999@gmail.com>
2021-08-09 02:00:38 +0530

    removed skills section in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
 import { Layout, Seo } from "components/common"
-import { Intro, Skills, RecentPosts } from "components/landing"
+import { Intro, RecentPosts } from "components/landing"
 const Home = () => (
   <Layout>
     <Seo />
     <Intro />
     <RecentPosts />
-    <Skills />
   </Layout>
 )
 
